bb74592b71e7222460a7ab7b1f5845977a704926
Haukur Kristinsson <haukur@hauxi.is>
2014-08-22 21:56:04 +0000

    Fixed timestamp bug, which caused us to read past quakes again.

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ while True:
     for entry in data["results"]:
         entry_time = parser.parse(entry["timestamp"])
         if latest < entry_time:
+			latest = entry_time
             if evals.eval_quake(entry) is not None:
                 logger.info("[Quake Threshold Reached]: " + str(entry))
                 quakewatcher.quakeOccured(entry)  # Let components know.
