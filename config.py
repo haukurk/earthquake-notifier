eb38fe45ecfe53c846c221cebdaea28d0ea971b8
Haukur Kristinsson <haukur@hauxi.is>
2014-09-03 21:44:09 +0000

    Bugfixes and logging improvement.

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # Web service poller settings
 DELAY_BETWEEN_CHECKS = 30  # Seconds.
 QUAKE_SIZE_THRESHOLD = 3.0
+SCANOLDQUAKES_DAYS = 1  # Do not ignore old quakes on startup (DAYS)
 
 # Email settings
 EMAIL_SENDER = "quake@hauxi.is"
@@ -9,5 +10,6 @@ EMAIL_SMTP_SERVER = "smtp.samskip.is"
 EMAIL_RECIPENTS = [
     {"name": "Haukur", "email": "haukur@hauxi.is"},
     {"name": "Darri", "email": "darri.helgason@samskip.com"},
-    {"name": "Trausti", "email": "trausti.jonsson@samskip.com"}
+    {"name": "Trausti", "email": "trausti.jonsson@samskip.com"},
+    {"name": "Arnar Jons", "email": "arnar@arnarjons.is"}
 ]
--- quakenotify.py
+++ quakenotify.py
@@ -1,6 +1,6 @@
 import json
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import requests
 from dateutil import parser
@@ -17,7 +17,7 @@ print "Starting Quake Notifier..."
 logger.info("Quake notify poller started!")
 
 # Save time for now, to skip old quakes.
-latest = datetime.now(pytz.utc)
+latest = datetime.now(pytz.utc)-timedelta(days=config.SCANOLDQUAKES_DAYS)
 print "Skipping quakes that occurred after: " + str(latest)
 
 # parse from apis.is
@@ -47,10 +47,9 @@ while True:
         for entry in data["results"]:
             entry_time = parser.parse(entry["timestamp"])
             if latest < entry_time:
-
                 if timeupdate is None:
                     timeupdate = entry_time
-                elif entry_time > timeupdate:
+                if entry_time > timeupdate:
                     timeupdate = entry_time
 
                 if evals.eval_quake(entry) is not None:
@@ -60,14 +59,15 @@ while True:
                     logger.info("[Quake Detected - Threshold higher than size ("+str(entry["size"])+")]")
 
         # Update time for next run.
-        latest = timeupdate
+        if timeupdate is not None:
+            latest = timeupdate
 
     # Handle Connection Exception
     except requests.ConnectionError, e:
-        logger.error('EXCEPTION: ConnectionError: ', e)
+        logger.error('EXCEPTION: ConnectionError: '+ str(e.message))
         continue
-    except:
-        logger.error('EXCEPTION: UNKNOWN Error')
+    except Exception, e:
+        logger.error('EXCEPTION: UNKNOWN Error: '+ str(e.message))
         continue
 
     # Idle between tries.
